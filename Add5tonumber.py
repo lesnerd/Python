4f7bf05b1df87ed7f910c31448924382cdd9d839
lesnerd <lesnerd@gmail.com>
2019-12-08 16:16:25 +0200

    fix function edge cases

--- Add5tonumber.py
+++ Add5tonumber.py
@@ -2,31 +2,32 @@ def solution(N):
     retString = list()
     added = 0
     intStringified = list(str(N))
+    isNegative = 0
     for n in intStringified:
-        if n == '-':
-            return int('-5' + ''.join(intStringified[1:]))
-        if int(n) <= 5:
+        if n == '-' or isNegative == 1:
+            isNegative = 1
+            if n == '-':
+                retString.append(n)
+                continue
+            elif int(n) >= 5 and added == 0:
+                retString.append(str(5))
+                added = 1
+            retString.append(n)
+            # return int('-5' + ''.join(intStringified[1:]))
+        elif int(n) <= 5:
             if added == 0:
                 retString.append(str(5))
                 added = 1
-        retString.append(n)
+            retString.append(n)
+        else:
+            retString.append(n)
+
+    if added == 0:
+        retString.append(str(5))
     return int(''.join(retString))
 
-def solutionImutable(N): # Not optimized solution 
-    retString = ''
-    added = 0
-    intStringified = str(N)
-    for n in intStringified:
-        if n == '-':
-            return int('-5' + intStringified[1:])
-        if int(n) <= 5:
-            if added == 0:
-                retString = retString + str(5)
-                added = 1
-        retString = retString + n
-    return int(retString)
 
-
-print(solution(268))
-print(solution(670))
-print(solution(-999))
+print(solution(268))  # 5268
+print(solution(670))  # 6750
+print(solution(-999))  # -5999
+print(solution(-444))  # -4445
