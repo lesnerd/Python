61b1a684764b8eed104b563f7176236f69ba62ff
David Lesner <lesnerd@gmail.com>
2019-12-17 02:37:26 +0200

    More string manipulation

--- ReverseEachWordInSentence.py
+++ ReverseEachWordInSentence.py
@@ -8,6 +8,22 @@ def reverseWordInSentence(sentence):
 
     return " ".join(newWords)
 
+def reverseTheWholeString(sentence):
+    if sentence is None:
+        return None
 
+    newWord = ''
+    for i in range(len(sentence) - 1, -1, -1):
+        newWord = newWord + sentence[i]
+    return newWord
+
+def reverseTheWordsOnly(sentence):
+    if sentence is None:
+        return None
+
+    words = sentence.split(" ")
+    return " ".join(words[::-1])
 
 print(reverseWordInSentence("I am trying to solve this problem."))
+print(reverseTheWholeString("This is microsoft")) # Should print: tfosorcim si sih
+print(reverseTheWordsOnly("This is microsoft")) # Should print: microsoft is this
