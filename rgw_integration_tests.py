32bc9eb36f0adac8a5ff53f1446f1b2bf36880bc
David Lesner <lesnerd@gmail.com>
2020-04-30 01:29:51 +0300

    Complete task

--- rgw_integration_tests.py
+++ rgw_integration_tests.py
@@ -1,17 +1,33 @@
 import unittest
-import pip._vendor.requests
+import requests
 import time
 import logging
 import json
 
-logging.basicConfig(filename='pyrgw.log',
-                    format='%(name)s %(levelname)s %(message)s')
+logging.basicConfig(filename='pyrgw.log', format='%(name)s %(levelname)s %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+class Tools(object):
+
+    def print_request(self, req):
+        print('============ Request ============\n')
+        print('HTTP/1.1 {method} {url}\n{headers}\n\n{body}'.format(
+            method=req.method,
+            url=req.url,
+            headers='\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+            body=json.dumps(json.loads(req.body.decode("utf-8") if req.body is not None else '{}'), indent=4),
+        ))
+
+    def print_response(self, res):
+        print('============ Response ============\n')
+        print('HTTP/1.1 {status_code}\n{headers}\n\n{body}'.format(
+            status_code=res.status_code,
+            headers='\n'.join('{}: {}'.format(k, v) for k, v in res.headers.items()),
+            body=json.dumps(json.loads(res.content.decode("utf-8")), indent=4),
+        ))
+
 # For reference https://requests.readthedocs.io/en/master/
-
-
 class TestRGW(unittest.TestCase):
 
     def __init__(self, methodName):
@@ -20,37 +36,16 @@ class TestRGW(unittest.TestCase):
         self.transaction_id = None
 
     def test_create_tranaction(self):
-        url = 'http://localhost/RetailGateway/api/selling/v1/saletransactions/4801?createNew=true'
+        url = 'http://192.168.1.108/retailgateway/api/selling/v1/saletransactions/4801?createNew=true'
         headers = {'Retailer': 'Retailer',
                    'TouchPoint': 'MobileShopper',
                    'StoreID': '110'}
-        r = pip._vendor.requests.put(url, headers=headers)
-        print_request(r.r)
-        print_response(r.Response)
-        # assert r.
-        # print('============ Response Header ============\n')
-        # for k, v in r.headers.items():
-        #     print('key: ' + k + ' value: ' + v)
-        # parsed = json.loads(r.text)
-        # print('============ Response Body ============\n')
-        # print(json.dumps(parsed, indent=4) + '\n')
-
-    def print_request(self, req):
-        print('HTTP/1.1 {method} {url}\n{headers}\n\n{body}'.format(
-            method=req.method,
-            url=req.url,
-            headers='\n'.join('{}: {}'.format(k, v)
-                              for k, v in req.headers.items()),
-            body=req.body,
-        ))
-
-    def print_response(self, res):
-        print('HTTP/1.1 {status_code}\n{headers}\n\n{body}'.format(
-            status_code=res.status_code,
-            headers='\n'.join('{}: {}'.format(k, v)
-                              for k, v in res.headers.items()),
-            body=res.content,
-        ))
+        r = requests.put(url, headers=headers)
+        tool = Tools()
+        tool.print_request(r.request)
+        tool.print_response(r)
+        parsed = json.loads(r.text)
+        assert parsed.get('SalesTransaction').get('TransactionStatus') == 'InProcess'
 
 
 if __name__ == '__main__':
