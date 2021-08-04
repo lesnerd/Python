028152f9ba881120c61137f6b2b16eaa77d65ebb
David Lesner <lesnerd@gmail.com>
2021-08-04 22:46:41 +0300

    complete

--- largest_sum_of_non_adjacent_numbers.py
+++ largest_sum_of_non_adjacent_numbers.py
@@ -4,7 +4,9 @@ This problem was asked by Airbnb.
 Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. 
 Numbers can be 0 or negative.
 
-For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, 
+For example, 
+[2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. 
+[5, 1, 1, 5] should return 10, 
 since we pick 5 and 5.
 '''
 
@@ -15,30 +17,45 @@ def solution(n):
         return n[0]
     if len(n) == 2:
         return max(n[0], n[1])
-    size = len(n)
-    x1, x2, x3 = 0, 1, 2
-    last = -1
-    end1, end2, end3 = False, False, False
+    if len(n) == 3:
+        return max(n[0] + n[2], n[1])
+    size = len(n) - 1
+    x1, x2, x3, x4 = 0, 1, 2, 3
+    last = -1000
     ret = 0
     while x1 < size:
-        if n[x1] + n[x3] > n[x2]:
+        if n[x1] + n[x3] > n[x2] and n[x3] > n[x4] and last + 1 != x1:
             ret += n[x1] + n[x3]
             last = x3
-        else:
+        elif n[x2] > n[x1]:
             ret += n[x2]
-            :w
             last = x2
+        else:
+            ret += n[x1]
+            last = x1
 
-        if x1 + 3 >= size:
+        if x1 + 3 <= size:
             x1 += 3
         else:
-            end1 = True
-        if x2 + 3 >= size:
+            return ret
+        
+        if x2 + 3 <= size:
             x2 += 3
         else:
-            end2 = True
-        if x3 + 3 >= 3:
+            if last + 1 == x1:
+                return ret
+            else:
+                ret + n[x1]
+        
+        if x3 + 3 <= 3:
             x3 += 3
         else:
-            end3 = True
+            if last + 1 == x1:
+                return ret + n[x2]
+            else:
+                return ret + max(n[x1], n[x2])
+    return ret
 
+
+print(solution([2, 4, 6, 2, 5]))
+print(solution([5, 1, 1, 5]))
